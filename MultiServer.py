d4385df9a6cd0f19f8d177382c36e7f532082b51
bkarjoo <bkarjoo@gmail.com>
2017-12-04 16:45:53 -0500

    20171204

--- MultiServer.py
+++ MultiServer.py
@@ -1,71 +1,128 @@
-from socket import *
-# Importing all from thread
-from thread import *
-from threading import *
+import socket
+import thread
+import threading
 import time
 import Queue
 
-# Defining server address and port
-host = ''  # 'localhost' or '127.0.0.1' or '' are all same
-port = 52000  # Use port > 1024, below it all are reserved
-quit_program = False
-# Creating socket object
-sock = socket()
-# Binding socket to a address. bind() takes tuple of host and port.
-sock.bind((host, port))
-# Listening at the address
-sock.listen(5)  # 5 denotes the number of clients can queue
 
-conns = []
+class MultiServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        self.connections = []
+        self.incoming_queue = Queue.Queue()
+        self.outgoing_queue = Queue.Queue()
+        self.quit_time = False
+        self.receiver = 0
 
-request_queue = Queue.Queue()
+    def incoming_message(self, message):
+        self.incoming_queue.put(message)
 
-def clientthread():
-    # infinite loop so that function do not terminate and thread do not end.
-    while True:
-        # Sending message to connected client
-        for c in conns:
-            c.send('Hi! I am server\n')  # send only takes string
-        # Receiving from client
-        #data = conn.recv(1024)  # 1024 stands for bytes of data to be received
-        #print data
-        time.sleep(5)
+    def get_enqueue_callback(self):
+        return self.incoming_message
 
-def clientlisten(conn):
-    while True:
-        request = conn.recv(1024)  # 1024 stands for bytes of data to be received
-        request_queue.put(request)
+    def set_receiver(self, receiver):
+        self.receiver = receiver
 
-def process_request_queue():
-    while True:
-        try:
-            message = request_queue.get(block=True, timeout=1)
-            print message
-            request_queue.task_done()
-            if quit_program:
+    def process_incoming(self):
+        # process the incoming queue pass messages to clients with .001 delay
+        while 1:
+            try:
+                message = self.incoming_queue.get(block=True, timeout=1)
+                for c in self.connections:
+                    try:
+                        c.send(message)
+                        time.sleep(.001)
+                    except:
+                        pass #if client issue don't do nothing with that client
+                self.incoming_queue.task_done()
+                if self.quit_time:
+                    break
+            except Queue.Empty:
+                if self.quit_time:
+                    break
+
+    def process_outgoing(self):
+        # process ourgoing queues pass messages to hydra with .005 delay
+        while 1:
+            try:
+                message = self.outgoing_queue.get(block=True, timeout=1)
+                if self.receiver:
+                    self.receiver(message)
+                self.outgoing_queue.task_done()
+                if self.quit_time:
+                    break
+            except Queue.Empty:
+                if self.quit_time:
+                    break
+
+    def client_listener(self, conn):
+        # each client gets one of these
+        while True:
+            try:
+                if self.quit_time: break
+                conn.settimeout(1)
+                request = conn.recv(1024)  # 1024 stands for bytes of data to be received
+                if len(request) == 0:
+                    conn.close()
+                    break
+                if request.upper() == 'QUIT':
+                    conn.close()
+                    break
+                self.outgoing_queue.put(request)
+            except socket.timeout:
+                pass
+            except:
                 break
-        except Queue.Empty:
-            if quit_program:
+
+    def listen_for_clients(self):
+        self.sock.settimeout(1)
+        while 1:
+            try:
+                if self.quit_time: break
+                conn, addr = self.sock.accept()
+                conn.setblocking(1)
+                self.connections.append(conn)
+                # Creating new thread. Calling clientthread function for this function and passing conn as argument.
+                thread.start_new_thread(self.client_listener,(
+                    conn, ))  # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+            except socket.timeout:
+                pass
+            except:
+                raise
+
+    def user_prompt(self):
+        while 1:
+            user_input = raw_input('>>')
+            if user_input.upper() == 'Q':
+                self.quit_time = True
                 break
+            for c in self.connections:
+                self.incoming_queue.put(user_input)
+        self.close()
+        print 'Multiserver is now closed.'
 
-t = Thread(target = clientthread)
-t.start()
-t2 = Thread(target = process_request_queue)
-t2.start()
+    def quit(self):
+        self.quit_time = True
+        self.close()
 
-while True:
-    # Accepting incoming connections
-    conn, addr = sock.accept()
-    # Creating new thread. Calling clientthread function for this function and passing conn as argument.
-    start_new_thread(clientlisten, (conn,))  # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    conns.append(conn)
+    def start(self):
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(5)
+        self.listener_thread = threading.Thread(target=self.listen_for_clients)
+        self.incoming_thread = threading.Thread(target=self.process_incoming)
+        self.outgoing_thread = threading.Thread(target=self.process_outgoing)
+        #self.console_thread = Thread(target=self.user_prompt)
+        self.incoming_thread.start()
+        self.outgoing_thread.start()
+        self.listener_thread.start()
+        #self.console_thread.start()
 
-for c in conns:
-    c.close()
-
-sock.close()
-
-t.join()
-t2.join()
-
-print 'DONE!'
+    def close(self):
+        self.incoming_thread.join()
+        self.outgoing_thread.join()
+        self.listener_thread.join()
+        for c in self.connections:
+            c.close()
+        self.sock.close()
--- Program.py
+++ Program.py
@@ -6,6 +6,7 @@ import time
 import datetime
 import Queue
 from store import *
+import MultiServer
 
 quit_program = False
 es_msg_count = 0
@@ -19,21 +20,22 @@ is_sock = 0
 # connections to the clients
 es_connection = 0
 is_connection = 0
-es_queue = 0
-is_queue = 0
+# esq_from_hydra = Queue.Queue()
+# esq_to_hydra = Queue.Queue()
+#
+# isq_from_hydra = Queue.Queue()
+# isq_to_hydra = Queue.Queue()
+
+esms = 0
+isms = 0
 
 
 def es_message_handler(message):
-    if es_connection:
-        # TODO :
-        #send message to the client
-        es_connection.sendall(message)
+    esms.incoming_message(message)
 
 
 def is_message_handler(message):
-    if is_connection:
-        # send message to the client
-        is_connection.sendall(message)
+    isms.incoming_message(message)
 
 
 def es_cleanup_callback():
@@ -42,6 +44,14 @@ def es_cleanup_callback():
 
 def is_cleanup_callback():
     pass
+
+
+def es_request(message):
+    ES.SendMessage(message)
+
+
+def is_request(message):
+    IS.SendMessage(message)
 
 
 def interactive_loop():
@@ -54,6 +64,8 @@ def interactive_loop():
         if command == 'quit' or command == 'q':
             if ES.IsConnected():
                 ES.Exit()
+            isms.quit()
+            esms.quit()
             global quit_program
             quit_program = True
             break
@@ -83,17 +95,6 @@ def interactive_loop():
             si = SessionInfo(addressof(hsi), es_handler, is_handler, es_cleanup, is_cleanup)
             hapi = HAPI_DLL(".\\HAPIKIT.DLL", si)
             hapi.Run()
-
-        if command == 'r':
-            hsi = HySessionInfo(b"algogroup", b"algogroup", b"10.17.240.155", 8701)
-            es_handler = SessionInfo.fnMsgHdlr_t(es_message_handler)
-            is_handler = SessionInfo.fnMsgHdlr_t(is_message_handler)
-            es_cleanup = SessionInfo.fnCleanUp_t(es_cleanup_callback)
-            is_cleanup = SessionInfo.fnCleanUp_t(is_cleanup_callback)
-
-            si = SessionInfo(addressof(hsi), es_handler, is_handler, es_cleanup, is_cleanup)
-            hapi = HAPI_DLL(".\\HAPIKIT.DLL", si)
-            hapi.dll.PostAdmMsgES(1001, 0, 0)
 
         if command == 'demo' or command == 'd':
             hsi = HySessionInfo(b"demo", b"demo", b"10.17.240.159", 7620) 
@@ -130,234 +131,18 @@ def heartbeat_loop():
         sleep(5)
 
 
-def accept_new_connection(sock):
-    while True:
-        if quit_program:
-            return None
-        try:
-            sock.settimeout(0.2)
-            sock.listen(1)
-            conn, addr = sock.accept()
-            return conn
-        except socket.timeout:
-            continue
-
-
-def read_request(conn):
-    while True:
-        if quit_program:
-            break
-        elif conn:
-            try:
-                if quit_program:
-                    break
-                header = conn.recv(13)
-                sleep(0.1)
-                if header:
-                    toks = header.split(':')
-                    length = int(toks[3]) - 13
-                    remainder = conn.recv(length)
-                    whole_message = header + remainder
-                    response = 'sending: ' + whole_message
-                    conn.sendall(response)
-                    return whole_message
-            except socket.timeout:
-                continue
-            except socket.error as err:
-                print 'es: ', err
-                conn.close()
-
-
-def write_response(conn, msg):
-    try:
-        if conn:
-            conn.sendall(msg)
-    except:
-        print 'Error in write_response'
-        print conn
-        print sys.exc_info()[0]
-
-
-def listen_for_es_connections():
-    global es_sock
-    global es_connection
-
-    while (not quit_program):
-        if es_connection == 0:
-            try:
-                es_sock.settimeout(0.2)
-                es_sock.listen(1)
-                es_connection, client_address = es_sock.accept()
-                es_connection.setblocking(1)
-                t = threading.Thread(target=listen_for_es_messages())
-                t.start()
-            except socket.timeout:
-                if quit_program:
-                    return
-        else:
-            sleep(1)
-            continue
-
-
-def listen_for_is_connections():
-    global is_sock
-    global is_connection
-
-    while (not quit_program):
-        if is_connection == 0:
-            try:
-                is_sock.settimeout(0.2)
-                is_sock.listen(1)
-                is_connection, client_address = is_sock.accept()
-                is_connection.setblocking(1)
-                t = threading.Thread(target=listen_for_is_messages())
-                t.start()
-            except socket.timeout:
-                if quit_program:
-                    return
-        else:
-            sleep(1)
-            continue
-
-
-def listen_for_es_messages():
-    global es_connection
-    while True:
-        if quit_program:
-            break
-        elif es_connection:
-            try:
-                header = es_connection.recv(13)
-                if header:
-                    length = int(header[10:13]) - 13
-                    remainder = es_connection.recv(length)
-                    whole_message = header + remainder
-                    if whole_message[14:] == 'QUIT':
-                        print 'closing es connection.'
-                        es_connection.close()
-                        es_connection = 0
-                        return
-                    ES.SendMessage(whole_message)
-
-                if quit_program:
-                    break
-            except socket.error as err:
-                print 'es: ', err
-                break
-            except:
-                print 'error in processing es message'
-
-    es_connection.close()
-    es_connection = 0
-
-
-def listen_for_is_messages():
-    global is_connection
-    while True:
-        if quit_program:
-            break
-        elif is_connection:
-            try:
-                header = is_connection.recv(13)
-                if header:
-                    length = int(header[10:13]) - 13
-                    remainder = is_connection.recv(length)
-                    whole_message = header + remainder
-                    if whole_message[14:] == 'QUIT':
-                        print 'closing is Connection.'
-                        is_connection.close()
-                        is_connection = 0
-                        return
-                    IS.SendMessage(whole_message)
-
-                if quit_program:
-                    break
-            except socket.error as err:
-                print 'is: ', err
-                break
-            except:
-                print 'error in processing is message'
-
-    is_connection.close()
-    is_connection = 0
-
-
-def create_server_socket(address):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', address)
-    sock.bind(server_address)
-    return sock
-
-
-def create_es_server_sock():
-    global es_sock
-    es_sock = create_server_socket(10000)
-    while True:
-        global es_connection
-        es_connection = accept_new_connection(es_sock)
-        try:
-            while True:
-                client_request = read_request(es_sock)
-                ES.SendMessage(client_request)
-        except socket.error as err:
-            print 'is: ', err
-        finally:
-            is_connection.close()
-
-
-def create_is_server_sock():
-    global is_sock
-    is_sock = create_server_socket(10001)
-    while True:
-        global is_connection
-        is_connection = accept_new_connection(is_sock)
-        try:
-            while True:
-                client_request = read_request(is_sock)
-                ES.SendMessage(client_request)
-        except socket.error as err:
-            print 'es: ', err
-        finally:
-            es_connection.close()
-
-
-def process_es_queue():
-    while True:
-        try:
-            message = es_queue.get(block=True, timeout=1)
-            write_response(es_connection, message)
-            insert_message_into_es(message)
-            es_queue.task_done()
-            if quit_program:
-                break
-        except Queue.Empty:
-            if quit_program:
-                break
-
-
-def process_is_queue():
-    while True:
-        try:
-            message = is_queue.get(block=True, timeout=1)
-            write_response(is_connection, message)
-            insert_message_into_is(message)
-            is_queue.task_done()
-            if quit_program:
-                break
-        except Queue.Empty:
-            if quit_program:
-                break
-
-
 if __name__ == "__main__":
+
+
     es_queue = Queue.Queue()
     is_queue = Queue.Queue()
-    es_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 10000)
-    es_sock.bind(server_address)
-    is_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 10001)
-    is_sock.bind(server_address)
+
+    esms = MultiServer.MultiServer('', 10000)
+    isms = MultiServer.MultiServer('', 10001)
+    esms.set_receiver(es_request)
+    isms.set_receiver(is_request)
+    esms.start()
+    isms.start()
 
     print 'starting threads'
     t1 = threading.Thread(target=interactive_loop)
@@ -366,27 +151,14 @@ if __name__ == "__main__":
     t2 = threading.Thread(target=heartbeat_loop)
     t2.start()
     print 'heartbeat started'
-    t3 = threading.Thread(target=listen_for_is_connections)
-    t3.start()
-    print 'IS socket listener started'
-    t4 = threading.Thread(target=listen_for_es_connections)
-    t4.start()
-    print 'ES socket listener started'
-    t5 = threading.Thread(target=process_es_queue)
-    t5.start()
-    t6 = threading.Thread(target = process_is_queue())
-    t6.start()
+
+
 
     print 'waiting for joins'
     t1.join()
     print 'interactive done'
     t2.join()
     print 'heartbeat done'
-    t3.join()
-    print 'IS done'
-    t4.join()
-    print 'ES done'
-    t5.join()
-    t6.join()
+
     print('ALL DONE')
 
